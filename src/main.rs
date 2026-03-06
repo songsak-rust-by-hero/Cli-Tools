c2c643b57975b59e6782772ac393fd65d5250555
YourNickname <nickname@study.local>
2026-03-06 22:55:22 +0700

    feat: implement CLI subcommands for listing and adding users

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,54 @@
+use anyhow::{Context, Result};
 use clap::Parser;
-use serde::{Serialize,Deserialize};
+use serde::{Deserialize, Serialize};
 use std::fs;
-use anyhow::{Result,Context};
 
 #[derive(Parser)]
-struct Cli{
+enum Cli {
+    List,
+    Add {
+        name: String,
+        age: u64,
+        email: String,
+    },
+}
+#[derive(Serialize, Deserialize)]
+struct UserData {
     name: String,
     age: u64,
     email: String,
 }
-#[derive(Serialize,Deserialize)]
-struct UserData{
-    name:String,
-    age:u64,
-    email:String
-}
-impl From<Cli> for UserData {
-    fn from (args:Cli)-> Self {
-        Self { name: args.name,
-        age:args.age,
-        email:args.email,
-        }
-    }
-}
-impl UserData{
-    fn save(&self)-> Result<()>{
-        let json = serde_json::to_string_pretty(self).context("JsonError")?;
-        std::fs::write("user.json",json).context("ErrorFile")?;
+
+impl UserData {
+    fn save_all(users: &Vec<Self>) -> Result<()> {
+        let json = serde_json::to_string_pretty(users).context("JsonError")?;
+        std::fs::write("user.json", json).context("ErrorFile")?;
         Ok(())
     }
 
-    fn load()-> Result<Self>{
+    fn load_all() -> Result<Vec<Self>> {
         let content = fs::read_to_string("user.json").context("Error not file")?;
-        let user: Self =
-            serde_json::from_str(&content).context("Errorserde")?;
-        Ok(user)
+        let users: Vec<Self> = serde_json::from_str(&content).context("Errorserde")?;
+        Ok(users)
+    }
 }
-}
-fn main()->anyhow::Result<()> {
-    match UserData::load() {
-        Ok(user) => { println!("Fond :{}(Age{}):(email) {}",user.name,user.age,user.email);
+fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
+    match args {
+        Cli::List => {
+            let users = UserData::load_all().unwrap_or_default();
+            println!("--------All names-------------");
+            for (i, u) in users.iter().enumerate() {
+                println!("{}.{}|{}|{}", i + 1, u.name, u.age, u.email);
+            }
         }
-        Err(_) =>{ println!("No existing user file found, proceeding to create a new one.");
+        Cli::Add { name, age, email } => {
+            let mut users = UserData::load_all().unwrap_or_default();
+            let new_user = UserData { name, age, email };
+            users.push(new_user);
+            UserData::save_all(&users)?;
+            println!("Flnish");
+        }
     }
-    }
-   let args = Cli::parse();
-   let user = UserData::from(args);
-   user.save().context("Error")?;
-   println!("finish");
-   Ok(())
+    Ok(())
 }
